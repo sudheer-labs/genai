d83e0eb54dff62ee01141fbbd817e81aa5f0019c
sudheer <saisudheerkonduri1@gmail.com>
2026-01-28 21:58:34 +0530

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+python --version
+py --version
+
+py -m venv .venv
+.venv\Scripts\activate
+python.exe -m pip install --upgrade pip
+pip install --force-reinstall -r requirements.txt -v
---
+++ test.py
@@ -0,0 +1,2 @@
+a = 5
+print(f"This is a round {a}")
